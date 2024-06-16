160344887782f900f49d18402465c0315ff7bdb8
gbdazevedo <162382383+gbdazevedo@users.noreply.github.com>
2024-06-16 14:28:15 -0300

    Create cadastrar.php

---
+++ cadastrar.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+    <head>
+
+        <meta charset="UTF-8">
+        <title>Documento</title>
+        <link rel="stylesheet" href="css/estilo.css"
+    
+    </head>
+
+    <body>
+        <div id="form-cad">
+
+            <h1>Cadastrar/h1>
+
+            <form method="post">
+ 
+                <input type="text" placeholder="Nome Completo" maxlength="80" name="nome">
+                <input type="text" placeholder="Telefone" maxlength="20" name="telefone">
+                <input type="email" placeholder="Usuário" name="email" maxlength="30">
+                <input type="password" placeholder="Senha" name="senha" maxlength="6">
+                <input type="password" placeholder="Confirmar Senha" maxlength="6">
+                <input type="submit" value="Cadastrar" id="cadastrar">
+            
+            </form>
+        </div>
+    
+    </body>
+
+</html>
